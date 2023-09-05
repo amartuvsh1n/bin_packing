a84f185e413056f203047789293cdfdd0765cdfb
amartuvsh1n <48076179+amartuvsh1n@users.noreply.github.com>
2023-09-06 01:24:07 +0800

    :bug: list to array out[]

--- main.py
+++ main.py
@@ -29,3 +29,17 @@ maxVol=12
 out = binpacking.to_constant_volume(a,maxVol)
 # write to output.xlsx
 pd.DataFrame(out).to_excel('output.xlsx', header=False, index=False)
+
+
+import numpy as np
+
+# Find the maximum length among all sublists
+max_length = max(len(sublist) for sublist in out)
+
+# Pad shorter sublists with zeros to make them the same length
+padded_list = [sublist + [0] * (max_length - len(sublist)) for sublist in out]
+
+# Convert the padded list to a NumPy array
+my_array = np.array(padded_list)
+
+print(type(my_array))
